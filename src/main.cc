f08d5800886357f6e724f19999c267ca7dae7406
Mikhail Mamaev <mam.mih.val@gmail.com>
2026-03-18 10:45:06 +0300

    Testing availability of EOS

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,14 @@
 #include <memory>
 #include <string>
+#include <functional>
 
 #include <TCanvas.h>
 #include <TLatex.h>
+#include <TFile.h>
 
 int main( int argc, char* argv[] ){
   auto str_name = std::string{ argv[1] }.append(".png");
+  auto str_file_name = std::string{ argv[2] };
   auto c = std::unique_ptr<TCanvas>{new TCanvas("canv", "canv", 800, 600)};
   c->cd();
   auto text = std::unique_ptr<TLatex>{new TLatex(0.25, 0.2, "fuck.")};
@@ -14,6 +17,10 @@ int main( int argc, char* argv[] ){
   text->Draw();
   c->Update();
   c->Print( str_name.c_str() );
+
+  auto file = std::unique_ptr<TFile, std::function<void(TFile*)>>{ TFile::Open(str_name.c_str(), "read"), [](auto* f){ f->Close(); }};
+  if( !file )
+    return 9;
 
   return 111;
 }
